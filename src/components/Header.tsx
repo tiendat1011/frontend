86ab9a926b3168bf6bf015117055b782d0e26100
tiendat1011 <tiendat9tc@gmail.com>
2024-10-26 16:22:54 +0700

    set up env for frontend

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return (
+        <div className="bg-blue-800 py-6"></div>
+    )
+}
---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,7 @@
+const Layout = () => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            
+        </div>
+    )
+}
